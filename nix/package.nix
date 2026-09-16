aae27f4f46cc3991906c0963093d0f716a2454ca
Mike Shepherd <mikeshepherd@users.noreply.github.com>
2026-09-16 18:22:53 +0100

    Fix package hash

--- nix/package.nix
+++ nix/package.nix
@@ -3,6 +3,6 @@ pkgs.buildGoModule {
   pname = "opnix";
   version = "0.10.1";
   src = ../.;
-  vendorHash = "sha256-H1v3SmLSrKgIUJInloLrFKTECddhZtBomFyIb8aqFzk=";
+  vendorHash = "sha256-++BUJ8vDV9N+sqhmKdqopvapDDJiaj/5ZbUfT8mX+cg=";
   subPackages = ["cmd/opnix"];
 }
